c001c21842b1a42a9781f23fcfff5949378a5890
Germain Loret <germain.loret@gmail.com>
2018-04-27 17:22:49 +0200

    update

--- task.rb
+++ task.rb
@@ -1,35 +1,47 @@
 class Task
-  attr_accessor :id, :title, :description, :done
-  def initialize(attributes = {})
-    @id = attributes["id"]
-    @title = attributes["title"]
-    @description = attributes["description"]
-    @done = attributes["done"] || 0
+  attr_reader :id
+  attr_accessor :title, :description, :done
+  def initialize(attributes)
+    @id = attributes[:id]
+    @title = attributes[:title]
+    @description = attributes[:description]
+    @done = attributes[:done] || false
   end
 
   def self.find(id)
-    result = DB.execute("SELECT * FROM tasks WHERE id = #{id}").first
+    result = DB.execute("SELECT * FROM tasks WHERE id = ?", id).first
     if result.nil?
       nil
     else
-      Task.new(result)
+      build_task(result)
     end
   end
 
   def save
-    if self.id.nil?
-      DB.execute("INSERT INTO tasks (title, description, done) VALUES ('#{self.title}','#{self.description}',#{self.done})")
-      self.id = DB.last_insert_row_id
+    if id.nil?
+      DB.execute("INSERT INTO tasks (title, description, done) VALUES (?, ?, ?)", title, description, done ? 1 : 0)
+      @id = DB.last_insert_row_id
     else
-      DB.execute("UPDATE tasks SET title = '#{self.title}', description = '#{self.description}', done = #{self.done} WHERE id = #{self.id}")
+      DB.execute("UPDATE tasks SET title = ?, description = ?, done = ? WHERE id = ?", title, description, done ? 1 : 0, id)
     end
   end
 
   def self.all
-    DB.execute("SELECT * FROM tasks").map { |task_hash| Task.new(task_hash) }
+    DB.execute("SELECT * FROM tasks").map { |task_hash| build_task(task_hash) }
   end
 
   def destroy
-    DB.execute("DELETE FROM tasks WHERE id=#{self.id}")
+    DB.execute("DELETE FROM tasks WHERE id=?", id)
+  end
+
+  private
+
+  def self.build_task(row)
+    Task.new(
+      id: row["id"],
+      title: row["title"],
+      description: row["description"],
+      done: row["done"] == 1
+    )
   end
 end
--- test.rb
+++ test.rb
@@ -5,15 +5,15 @@ require_relative "task"
 
 # READ one
 task = Task.find(1)
-puts "#{task.done == 0 ? '[ ]' : '[X]'} #{task.title}: #{task.description}"
+puts "#{task.done ? '[X]' : '[ ]'} #{task.title}: #{task.description}"
 
-# CREATE
-new_task = Task.new("title" => 'Buy some beers', "description" => 'Quickly!')
+CREATE
+new_task = Task.new(title: 'Buy some beers', description: 'Quickly!')
 new_task.save
 p new_task
 
 # UPDATE
-task = Task.find(2)
+task = Task.find(5)
 task.title = "Make cookies"
 task.description = "With chocolate"
 task.save
@@ -21,9 +21,10 @@ task.save
 # READ all
 tasks = Task.all
 tasks.each do |task|
-  puts "#{task.done == 0 ? '[ ]' : '[X]'} #{task.title}: #{task.description}"
+  p task
+  puts "#{task.done ? '[X]' : '[ ]'} #{task.title}: #{task.description}"
 end
 
 # DELETE
-task = Task.find(3)
+task = Task.find(2)
 task.destroy
