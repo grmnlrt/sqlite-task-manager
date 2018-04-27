8310fd1acd020d701a15fb960c0444c9abf6abc6
Germain Loret <germain.loret@gmail.com>
2018-04-27 18:30:41 +0200

    batch-146 version

--- task.rb
+++ task.rb
@@ -1,1 +1,51 @@
+class Task
+  attr_writer :title
+  def initialize( attributes = {})
+    @id = attributes[:id]
+    @title = attributes[:title]
+    @description = attributes[:description]
+    @done = attributes[:done] || false
+  end
 
+  def self.find(id)
+    result = DB.execute("SELECT * FROM tasks WHERE id = ?", id)
+    hash = result.first
+    if hash.nil?
+      return nil
+    else
+      hash_to_task(hash)
+    end
+  end
+
+  def save
+    # 1. ID exists ?
+    # 2. If yes, update
+    # 3. If not, create
+    if @id.nil?
+      DB.execute("INSERT INTO tasks(title, description, done)
+      VALUES(?, ?, ?)", @title, @description, @done ? 1 : 0)
+      @id = DB.last_insert_row_id
+    else
+      DB.execute("UPDATE tasks SET title = ?, description = ?, done = ? WHERE id = ?", @title, @description, @done ? 1 : 0, @id)
+    end
+  end
+
+  def self.all
+    results = DB.execute("SELECT * FROM tasks")
+    results.map do |result|
+      hash_to_task(result)
+    end
+  end
+
+  def destroy
+    DB.execute("DELETE FROM tasks WHERE id = ?", @id)
+  end
+
+  private
+
+  def self.hash_to_task(result)
+    hash_final = {id: result["id"], title: result["title"], description: result["description"], done: result["done"] == 1}
+    Task.new(hash_final)
+  end
+
+end
--- test.rb
+++ test.rb
@@ -4,11 +4,21 @@ DB.results_as_hash = true
 require_relative "task"
 
 # READ one
+# task = Task.find(2)
+# p task
 
 # CREATE
-
+  # apero = Task.new(title: 'biere',description: 'au wagon')
+  # apero.save
 # UPDATE
+  # task = Task.find(3)
+  # task.title = "cider"
+  # task.save
 
 # READ all
+  # p Task.all
+
 
 # DELETE
+task = Task.find(1)
+task.destroy
